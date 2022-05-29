2efc734ecda9c12d51e44e26b235080ac7870b07
nick7ong <nnt25@miami.edu>
2022-05-28 23:53:00 -0400

    basic terminal GUI
    used open-source tabulate.hpp directory for table GUI. Trying to start storing guess words into a char array and then display into table GUI

--- nordle/Source.cpp
+++ nordle/Source.cpp
@@ -2,35 +2,117 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <cstring>
+#include "tabulate.hpp"
 
 using namespace std;
+using namespace tabulate;
 
-string file = "words.txt";
+// main library for words
+vector<string> WordLibrary;
 
-void fileStream(vector<string>& V, string f) {
-	ifstream fin;
-	fin.open(f);
-	if (fin.is_open()) {
-		while (!fin.eof()) {
-			string w;
-			fin >> w;
-			V.push_back(w);
+class GameWord {
+protected:
+	
+
+public:
+	// initiate filestream into vector
+	void FileStream(vector<string>& V, string f = "words.txt") {
+		ifstream fin;
+		fin.open(f);
+		if (fin.is_open()) {
+			while (!fin.eof()) {
+				string w;
+				fin >> w;
+				V.push_back(w);
+			}
+		}
+		else if (fin.fail()) {
+			cout << "file error" << endl;
+			exit(1);
+		}
+		fin.close();
+	}
+
+	void PrintLibrary() {
+		for (int i = 0; i < WordLibrary.size(); i++) {
+			cout << WordLibrary[i] << "\n";
 		}
 	}
-	else if (fin.fail()) {
-		cout << "file error" << endl;
-		exit(1);
+};
+
+string stoc(char c) {
+	string s(1, c);
+	return s;
+}
+
+void TerminalGUI(int turnRow)
+{
+	Table GUI;
+	string guess("");
+	char* prevGuess{};
+
+	if (turnRow == 0) {
+		GUI.add_row({ " ", " ", " ", " ", " " });
+		GUI.add_row({ " ", " ", " ", " ", " " });
+		GUI.add_row({ " ", " ", " ", " ", " " });
+		GUI.add_row({ " ", " ", " ", " ", " " });
+		GUI.add_row({ " ", " ", " ", " ", " " });
+		GUI.add_row({ " ", " ", " ", " ", " " });
+		cout << GUI << endl;
+		cout << "Enter Guess: ";
+		cin >> guess;
+		prevGuess = &guess[0];
+
 	}
-	fin.close();
+	else if (turnRow == 1) {
+		for (int i = 0; i < guess.length() + 1; i++) {
+			GUI[0][0].set_text(stoc(prevGuess[i]));
+		}
+		cout << "turn 2" << endl;
+		cout << "Enter Guess: ";
+		cin >> guess;
+	}
+	else if (turnRow == 2) {
+		cout << "turn 3" << endl;
+		cout << "Enter Guess: ";
+		cin >> guess;
+	}
+	else if (turnRow == 3) {
+		cout << "turn 4" << endl;
+		cout << "Enter Guess: ";
+		cin >> guess;
+	}
+	else if (turnRow == 4) {
+		cout << "turn 5" << endl;
+		cout << "Enter Guess: ";
+		cin >> guess;
+	}
+	else if (turnRow == 5) {
+		cout << "turn 6" << endl;
+		cout << "Enter Guess: ";
+		cin >> guess;
+	}
+	else if (turnRow == 6) {
+		cout << "GAME OVER!" << endl;
+	}
+
+}
+
+void IterateTurn(int turnCount) {
+	while (true) {
+		TerminalGUI(turnCount);
+		turnCount++;
+	}
 }
 
 
-
 int main() {
-	vector<string> V;
-	fileStream(V, file);
-	for (int i = 0; i < V.size(); i++) {
-		cout << V[i] << "\n";
-	}
+	GameWord N;
+	int turn = 0;
 
+	N.FileStream(WordLibrary);
+	IterateTurn(turn);
+
+	return 0;
 }
