ae2bb57891087dc9a150cb39640757a8dc5abee4
nick7ong <nnt25@miami.edu>
2022-06-01 13:02:15 -0400

    TerminalGUI() actually works now
    takes user input of 5 letter word guess for 6 turns and displays all guesses in their respective rows

--- nordle/Source.cpp
+++ nordle/Source.cpp
@@ -38,134 +38,170 @@ public:
 		}
 	}
 
+	string RandomWord(vector<string> V) {           //FIX THIS: doesnt actually generate random word
+		vector<string> currentVector = V;
+		string randomWord;
+		int randomNumber = rand() % currentVector.size();
+		randomWord = currentVector.at(randomNumber);
+		return randomWord;
+	}
+
 };
 
-struct Table {
+struct Element {
 	string border = "+---+---+---+---+---+";
-	array<char, 21> blankrow = { '|', '~', '~', '~', '|', '~', '~', '~', '|', '~', '~', '~', '|', '~', '~', '~', '|', '~', '~', '~', '|' };
-	array<char, 21> guessrow = { '|', '~', '~', '~', '|', '~', '~', '~', '|', '~', '~', '~', '|', '~', '~', '~', '|', '~', '~', '~', '|' };
+	array<char, 21> row = { '|', '~', '~', '~', '|', '~', '~', '~', '|', '~', '~', '~', '|', '~', '~', '~', '|', '~', '~', '~', '|' };
 
-	int blank = 6;
-	int guess = 0;
+	void PrintBlank(int rowNum) {
+		for (int i = 0; i < rowNum; i++) {
+			cout << border << '\n';
+			for (auto& i : row)
+				cout << i;
+			cout << '\n';
+		} cout << border << '\n';
 
-	/*void FileStream(vector<vector<char>>& V, string f = "NewTable.txt") { // store file int 2d vect or arr
-		ifstream fin;
-		fin.open(f);
-		if (fin.is_open()) {
-			while (!fin.eof()) {
-				char w;
-				fin >> w;
-				V.push_back(w);
+	}
+
+	void PrintGuess(Element r) {
+		for (auto& i : r.row)
+			cout << i;
+		cout << '\n';
+	}
+
+
+};
+
+void TerminalGUI(const string guessWord)
+{
+	cout << guessWord << '\n';
+	Element blank, r1, r2, r3, r4, r5, r6;
+	string g1, g2, g3, g4, g5, g6;
+	int turns = 6;
+	const int s = 1024;
+	char cg1[s], cg2[s], cg3[s], cg4[s], cg5[s], cg6[s];
+
+	for (int turnRow = 0; turnRow <= 6; turnRow++) {
+		if (turnRow == 0) {
+			blank.PrintBlank(turns - turnRow);
+			cout << "Enter Guess: ";
+			cin >> g1;
+			
+			strncpy_s(cg1, g1.c_str(), sizeof(cg1));
+			for (int i = 2, j = 0; i < r1.row.size() + 1 && j < 6; i += 4, j++) {
+				r1.row[i] = cg1[j];
+			}	
+		}
+		else if (turnRow == 1) {
+			cout << blank.border << '\n';
+			r1.PrintGuess(r1);
+			r2.PrintBlank(turns - turnRow);
+			cout << "turn 2" << endl;
+			cout << "Enter Guess: ";
+			cin >> g2;
+
+			strncpy_s(cg2, g2.c_str(), sizeof(cg2));
+			for (int i = 2, j = 0; i < r2.row.size() + 1 && j < 6; i += 4, j++) {
+				r2.row[i] = cg2[j];
 			}
 		}
-		else if (fin.fail()) {
-			cout << "file error" << endl;
-			exit(1);
+		else if (turnRow == 2) {
+			cout << blank.border << '\n';
+			r1.PrintGuess(r1);
+			cout << blank.border << '\n';
+			r2.PrintGuess(r2);
+			r3.PrintBlank(turns - turnRow);
+			cout << "turn 3" << endl;
+			cout << "Enter Guess: ";
+			cin >> g3;
+
+			strncpy_s(cg3, g3.c_str(), sizeof(cg3));
+			for (int i = 2, j = 0; i < r3.row.size() + 1 && j < 6; i += 4, j++) {
+				r3.row[i] = cg3[j];
+			}
 		}
-		fin.close();
-	}
-	*/
+		else if (turnRow == 3) {
+			cout << blank.border << '\n';
+			r1.PrintGuess(r1);
+			cout << blank.border << '\n';
+			r2.PrintGuess(r2);
+			cout << blank.border << '\n';
+			r3.PrintGuess(r3);
+			r4.PrintBlank(turns - turnRow);
+			cout << "turn 4" << endl;
+			cout << "Enter Guess: ";
+			cin >> g4;
 
-	void PrintGuess() {
-		for (int i = 0; i < guess; i++) {
-			cout << border << '\n';
-			for (auto& i : guessrow)
-				cout << i;
-			cout << '\n';
-		} cout << border << '\n';
-
-	}
-
-	void PrintBlank() {
-		for (int i = 0; i < blank; i++) {
-			cout << border << '\n';
-			for (auto& i : blankrow)
-				cout << i;
-			cout << '\n';
-		} cout << border << '\n';
-
-	}
-
-};
-
-
-
-
-
-void TerminalGUI(int turnRow)
-{
-	Table T;
-	string g1, g2, g3, g4, g5, g6;
-
-	if (turnRow == 0) {
-		
-		T.PrintBlank();
-		cout << "Enter Guess: ";
-		cin >> g1;
-
-		//get this to work global
-		char cg1[1024];
-		strncpy_s(cg1, g1.c_str(), sizeof(cg1));
-		cg1[sizeof(cg1) - 1] = 0;
-
-		for (int i = 2 , j = 0; i < T.guessrow.size() + 1 && j < 6; i += 4, j++) {
-			T.guessrow[i] = cg1[j];
+			strncpy_s(cg4, g4.c_str(), sizeof(cg4));
+			for (int i = 2, j = 0; i < r4.row.size() + 1 && j < 6; i += 4, j++) {
+				r4.row[i] = cg4[j];
+			}
 		}
+		else if (turnRow == 4) {
+			cout << blank.border << '\n';
+			r1.PrintGuess(r1);
+			cout << blank.border << '\n';
+			r2.PrintGuess(r2);
+			cout << blank.border << '\n';
+			r3.PrintGuess(r3);
+			cout << blank.border << '\n';
+			r4.PrintGuess(r4);
+			r5.PrintBlank(turns - turnRow);
+			cout << "turn 5" << endl;
+			cout << "Enter Guess: ";
+			cin >> g5;
 
-		
+			strncpy_s(cg5, g5.c_str(), sizeof(cg5));
+			for (int i = 2, j = 0; i < r5.row.size() + 1 && j < 6; i += 4, j++) {
+				r5.row[i] = cg5[j];
+			}
+		}
+		else if (turnRow == 5) {
+			cout << blank.border << '\n';
+			r1.PrintGuess(r1);
+			cout << blank.border << '\n';
+			r2.PrintGuess(r2);
+			cout << blank.border << '\n';
+			r3.PrintGuess(r3);
+			cout << blank.border << '\n';
+			r4.PrintGuess(r4);
+			cout << blank.border << '\n';
+			r5.PrintGuess(r5);
+			r6.PrintBlank(turns - turnRow);
+			cout << "turn 6" << endl;
+			cout << "Enter Guess: ";
+			cin >> g6;
 
-		T.blank--;
-		T.guess++;
-	}
-	else if (turnRow == 1) {
-		for (auto& i : T.guessrow)
-			cout << i;
-		T.PrintBlank();
-
-		cout << "turn 2" << endl;
-		cout << "Enter Guess: ";
-		cin >> g2;
-	}
-	else if (turnRow == 2) {
-		cout << "turn 3" << endl;
-		cout << "Enter Guess: ";
-		cin >> g3;
-	}
-	else if (turnRow == 3) {
-		cout << "turn 4" << endl;
-		cout << "Enter Guess: ";
-		cin >> g4;
-	}
-	else if (turnRow == 4) {
-		cout << "turn 5" << endl;
-		cout << "Enter Guess: ";
-		cin >> g5;
-	}
-	else if (turnRow == 5) {
-		cout << "turn 6" << endl;
-		cout << "Enter Guess: ";
-		cin >> g6;
-	}
-	else if (turnRow == 6) {
-		cout << "GAME OVER!" << endl;
-	}
-
-}
-
-void IterateTurn(int turnCount) {
-	while (true) {
-		TerminalGUI(turnCount);
-		turnCount++;
+			strncpy_s(cg6, g6.c_str(), sizeof(cg6));
+			for (int i = 2, j = 0; i < r6.row.size() + 1 && j < 6; i += 4, j++) {
+				r6.row[i] = cg6[j];
+			}
+		}
+		else if (turnRow == 6) {
+			cout << '\n';
+			cout << "GAME OVER!\n";
+			cout << blank.border << '\n';
+			r1.PrintGuess(r1);
+			cout << blank.border << '\n';
+			r2.PrintGuess(r2);
+			cout << blank.border << '\n';
+			r3.PrintGuess(r3);
+			cout << blank.border << '\n';
+			r4.PrintGuess(r4);
+			cout << blank.border << '\n';
+			r5.PrintGuess(r5);
+			cout << blank.border << '\n';
+			r6.PrintGuess(r6);
+			cout << blank.border << '\n';
+		}
 	}
 }
 
 
 int main() {
 	GameWord N;
-	int turn = 0;
 
 	N.FileStream(WordLibrary);
-	IterateTurn(turn);
+	TerminalGUI(N.RandomWord(WordLibrary));
 
 	return 0;
 }
